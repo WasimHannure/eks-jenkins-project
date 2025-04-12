bc174b34c68e8572a10e16f3988622ce9583f39e
WasimHannure <116272193+WasimHannure@users.noreply.github.com>
2025-04-12 17:51:48 +0530

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -32,7 +32,7 @@ def index(l):
     l.client.get("/")
 
 def setCurrency(l):
-    currencies = ['EUR', 'USD', 'JPY', 'CAD']
+    currencies = ['EUR', 'USD', 'JPY', 'CAD', 'INR']
     l.client.post("/setCurrency",
         {'currency_code': random.choice(currencies)})
 
